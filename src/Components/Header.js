03e0f896eaea10d41171eeeeb1ccb5138a52ef1d
bruktawit <bruktawit2wolde@gmail.com>
2022-08-09 22:40:45 -0400

    more works

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './Header.scss';
+import {images} from '../images';
 
 function Header() {
-  return (
-    <div>Header
+
+    const[open,setOpen] = useState(false);
+    const clickHnadler = (e) => {
+        e.preventDefault();
+        setOpen(!open)
+
+    }
+    return (
+    <div>
         <header>
             <nav className='navbar container'>
                 <div className='logo'>
-
+                    <img src={images.logo} alt=""/>
+                </div>
+                <ul className={open ? `nav-items active` : `nav-items`}>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Portfolio</li>
+                    <li className='btn btn--nav-btn'>View Plans</li>
+                </ul>
+                <div className='hamburger'>
+                    <img src={images.hamburger} alt="" onClick={clickHnadler}/>
                 </div>
             </nav>
         </header>
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './Hero.scss';
+import {images} from '../images';
+
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div>
+        <div className='hero'>
+            <div className='right-top-image'>
+                <picture>
+                    <source
+                    media="(max-width: 767px)"
+                    SrcSet={images.intro_right_mobile} />
+                    <img src={images.intro_right} alt="" />
+                </picture>
+            </div>
+            <div className='hero__wrapper container'>
+                <div className='hero__content'>
+                    <h1 className='title'> Humanizing <br/> Your insurance.</h1>
+                    <p className='hero__text'> Get your life insurance coverages easier and faster. We blend our experties and technology to help you find the plan that's right fpr you .Ensure you and your loved ones are protected/</p>
+                    <button className='btn'>VIEW PLANS</button>
+                </div>
+                <div className='hero__image'>
+            <picture>
+                    <source
+                    media="(max-width: 767px)"
+                    SrcSet={images.intro_mobile} />
+                    <img src={images.intro_desktop} alt="" />
+                </picture>
+            </div>
+            </div>
+            <div className='left-bottom-image'>
+            <picture>
+                    <source
+                    media="(max-width: 767px)"
+                    SrcSet={images.intro_left_mobile} />
+                    <img src={images.intro_left} alt="" />
+                </picture>
+            </div>
+        </div>
+    </div>
   )
 }
 
